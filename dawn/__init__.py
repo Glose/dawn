82c9c70463b9afc57207f0bf6e2487360db708e3
Arthur Darcet <arthur.darcet@m4x.org>
2014-10-23 17:51:01 +0200

    parse metadata for epub 2

--- dawn/__init__.py
+++ dawn/__init__.py
@@ -1,5 +1,6 @@
 import mimetypes
 import oset
+import uuid
 import xml.etree.ElementTree as ET
 import zipfile
 
@@ -8,7 +9,15 @@ SPECS = {}
 NS = {
 	'container': 'urn:oasis:names:tc:opendocument:xmlns:container',
 	'opf': 'http://www.idpf.org/2007/opf',
+	'dc': 'http://purl.org/dc/elements/1.1/',
 }
+
+def getxmlattr(tag, attr, fallback=None):
+	if ':' in attr:
+		ns, attr = attr.split(':', 1)
+		attr = '{{{}}}{}'.format(NS[ns], attr)
+	return tag.get(attr, fallback)
+
 
 class Epub(zipfile.ZipFile):
 	def __init__(self, infile, mode='r', opf=None, version=None):
@@ -18,13 +27,14 @@ class Epub(zipfile.ZipFile):
 				raise TypeError('Can\'t set a custom opf when opening an epub in read mode')
 			with super().open('META-INF/container.xml') as f:
 				tree = ET.parse(f)
-			self.opf = tree.find('./container:rootfiles/container:rootfile', NS).attrib['full-path']
-			self.version = version or self.getopf().attrib['version']
+			self.opf = tree.find('./container:rootfiles/container:rootfile', NS).get('full-path')
+			self.version = version or self.getopf().get('version')
 		elif mode == 'w':
 			if version is None:
 				raise TypeError('Version is required when opening an ePub in \'w\' mode')
-			self.version = version
 			self.opf = opf or 'content.opf'
+			self.version = version
+			self.uid = uuid.uuid4()
 		else:
 			raise TypeError('mode should be \'r\' or \'w\'')
 		
@@ -41,7 +51,9 @@ class Epub(zipfile.ZipFile):
 				self.spine.add(self.manifest.byiid(iid))
 			for href, title, children in self.spec.read_toc():
 				self.toc.append(href, title, children)
-			self.meta.update(self.spec.read_meta())
+			self.meta.update(dict(self.spec.read_meta()))
+			uid_id = self.getopf().get('unique-identifier')
+			self.uid = next(filter(lambda i: i.id == uid_id, self.meta['identifiers']), None)
 	
 	def __exit__(self, *args):
 		super().writestr('mimetype', b'application/epub+zip', compress_type=zipfile.ZIP_STORED)
@@ -164,19 +176,47 @@ class Spec:
 
 
 class Spec20(Spec):
+	class AttributeString(str):
+		pass
+	
 	def read_manifest(self):
 		for item in self.opf.findall('./opf:manifest/', NS):
-			yield item.attrib['id'], item.attrib['href']
+			yield item.get('id'), item.get('href')
 	
 	def read_spine(self):
 		for item in self.opf.findall('./opf:spine/', NS):
-			yield item.attrib['idref']
+			yield item.get('idref')
 	
 	def read_toc(self):
 		return []
 	
 	def read_meta(self):
-		return {}
+		metadata = self.opf.find('./opf:metadata', NS)
+		def extract(tags, attrs=(), only_one=False):
+			res = []
+			for item in metadata.findall(tags, NS):
+				res.append(self.AttributeString(item.text))
+				for k in attrs:
+					setattr(res[-1], k.split(':', 1)[-1].replace('-', '_'), getxmlattr(item, k))
+				if only_one:
+					return res[0]
+			return None if only_one else res
+		yield 'titles', extract('dc:title', ('lang',))
+		yield 'creators', extract('dc:creator', ('opf:role', 'opf:file-as'))
+		yield 'subjects', extract('dc:subject')
+		yield 'description', extract('dc:description', only_one=True)
+		yield 'publisher', extract('dc:publisher', only_one=True)
+		yield 'contributors', extract('dc:contributor', ('opf:role', 'opf:file-as'))
+		yield 'dates', extract('dc:date', ('opf:event',))
+		# Drop type
+		# Drop format
+		yield 'identifiers', extract('dc:identifier', ('id', 'opf:scheme'))
+		yield 'source', extract('dc:source', only_one=True)
+		yield 'languages', extract('dc:language')
+		# Drop relation
+		# Drop coverage
+		# Drop rights
+
 SPECS['2.0'] = Spec20
 
 
