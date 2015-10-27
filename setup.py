8bf99e6c22cc51c9d46304649a0b4727cc985ded
Arthur Darcet <arthur.darcet@m4x.org>
2015-10-27 16:03:42 +0100

    add MIT license

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
 	author='Arthur Darcet',
 	author_email='hello+dawn@glose.com',
 	url='http://github.com/Glose/dawn',
+	license='MIT',
 	packages=['dawn'],
 	install_requires=[
 		'lxml>=3.4.0',
@@ -18,7 +19,12 @@ setup(
 	classifiers=[
 		'Development Status :: 4 - Beta',
 		'Intended Audience :: Developers',
+		'License :: OSI Approved :: MIT License',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python',
 		'Programming Language :: Python :: 3.3',
+		'Programming Language :: Python :: 3.4',
+		'Programming Language :: Python :: 3.5',
 		'Topic :: Software Development :: Libraries :: Python Modules',
 	],
 )
