aca8cd29bcc0f779fe45dd9522abd33b0793064a
Arthur Darcet <arthur.darcet@m4x.org>
2015-10-27 16:03:52 +0100

    0.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
 	name='dawn',
-	version='0.1.1',
+	version='0.1.2',
 	description='Open and modify ePubs',
 	author='Arthur Darcet',
 	author_email='hello+dawn@glose.com',
